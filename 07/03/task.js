23c3c4fd124d000c221184e8e65af68bed2fda80
Cheshear <cheshearneko@gmail.com>
2017-11-09 23:23:39 +0300

    3 tasks

--- 07/03/task.js
+++ 07/03/task.js
@@ -2,8 +2,21 @@
  * Исправьте проблему с таймером: должны выводиться числа от 0 до 9.
  * Доп. задание: предложите несколько вариантов решения.
  */
-function throttle(time, callback) {
-  return callback;
+function throttle (time, callback){
+  let isTime = false;
+
+  return function(...args){
+    if (isTime) {
+      return;
+    }
+
+    callback(args);
+    isTime = true;
+
+    setTimeout(() => {
+      isTime = false;
+    }, time);
+  };
 }
 
 
